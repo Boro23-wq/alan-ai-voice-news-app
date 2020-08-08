88e3fc952a1c35c3ef29ec9167a6e735a5d8c40f
Boro23 <404slimboysavage@gmail.com>
2020-08-08 13:19:32 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,53 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
 
+import wordsToNumbers from 'words-to-numbers';
+
+import useStyles from './styles.js';
+
 const alanKey =
   'e3eee89f196b5ae2676c2f16333310e22e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const classes = useStyles();
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText('Please try again!');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          }
         }
       },
     });
   }, []);
   return (
     <div>
-      <h1>Alan AI News Application</h1>
-      <NewsCards articles={newsArticles} />
+      <div className={classes.logoContainer}>
+        <img
+          src='https://www.analyticsinsight.net/wp-content/uploads/2020/02/Savvycom-AI-Lab.jpg'
+          className={classes.alanLogo}
+          alt='logo'
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 };
